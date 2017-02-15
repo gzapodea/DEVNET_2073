afff078775f20d73c4a7c9bab81fdac88759313b
Gabriel Zapodeanu <gzapodea@cisco.com>
2017-02-15 11:42:41 -0800

    ready for Cisco Live

---
+++ config_data_2073.py
@@ -0,0 +1,24 @@
+# developed by Gabi Zapodeanu, Cisco Systems, TSA, GPO
+
+# !/usr/bin/env python3
+
+# Users need a Spark account
+
+# Spark declarations
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Paste your Spark token here'
+ROOM_NAME = 'DEVNET-2073-lab'
+IT_ENG_EMAIL = 'gabriel.zapodeanu@gmail.com'
+
+# APIC-EM declarations
+
+APIC_EM_URL = 'https://sandboxapic.cisco.com/api/v1'
+APIC_EM_USER = 'devnetuser'
+APIC_EM_PASSW = 'Cisco123!'
+
+# CMX declarations
+
+CMX_URL = 'https://msesandbox.cisco.com:8081/'
+CMX_USER = 'learning'
+CMX_PASSW = 'learning'
