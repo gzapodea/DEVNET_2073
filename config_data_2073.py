7c34566bbd1b0b4105377506ef7855d996db82f4
gzapodea <gzapodea@cisco.com>
2017-02-16 10:38:57 -0800

    ready for Cisco Live

--- config_data_2073.py
+++ config_data_2073.py
@@ -7,7 +7,7 @@
 # Spark declarations
 
 SPARK_URL = 'https://api.ciscospark.com/v1'
-SPARK_AUTH = 'Bearer ' + 'Paste your Spark token here'
+SPARK_AUTH = 'Bearer ' + 'NGQ1NjNkODYtYWFjOC00ZjkzLTljZjQtN2VlZTViZDA1YmZmNWM5ZWVhMTUtMTE4'
 ROOM_NAME = 'DEVNET-2073-lab'
 IT_ENG_EMAIL = 'gabriel.zapodeanu@gmail.com'
 
